948b3b5226897982ba052b48a1de0242e5ab5466
Tahina1 <92741906+Tahina1@users.noreply.github.com>
2023-05-08 22:52:43 +0300

    Update card.blade.php

--- resources/views/article/card.blade.php
+++ resources/views/article/card.blade.php
@@ -35,17 +35,6 @@
 
 
       </div>
-      <div class="datatable-bottom">
-        <nav aria-label="Page navigation example">
-            <ul class="pagination">
-              <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-              <li class="page-item"><a class="page-link" href="#">1</a></li>
-              <li class="page-item"><a class="page-link" href="#">2</a></li>
-              <li class="page-item"><a class="page-link" href="#">3</a></li>
-              <li class="page-item"><a class="page-link" href="#">Next</a></li>
-            </ul>
-        </nav>
-    </div>
     </section>
 
 </main><!-- End #main -->
